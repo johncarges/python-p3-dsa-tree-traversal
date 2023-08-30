edf071274bfe15944cb4f7428ea24f73ef31d81c
John Carges <johncarges@gmail.com>
2023-08-30 10:06:53 -0400

    adds get by id functionality

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,16 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_check = [self.root]
+    while len(nodes_to_check) > 0 :
+      node = nodes_to_check.pop(0)
+      if node["id"] == id:
+        return node
+      else:
+        nodes_to_check += node["children"]
+    return None
+
+
+
+
+
